5c3e47196ac587bbc75e386845d4859bbfa42461
Kons M <katashi.rain@gmail.com>
2024-02-20 11:36:40 +0900

    Started converting the API to work with the db

--- app/main.py
+++ app/main.py
@@ -1,120 +1,126 @@
 # Import fastapi and create a default test route
-from typing import Optional
+import sqlite3
+from pathlib import Path
+from typing import List
 
-import requests
-from fastapi import FastAPI, status
-from fastapi.exceptions import HTTPException
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.responses import JSONResponse
 from pydantic.main import BaseModel
+
+script_path = Path(__file__).parent
+project_path = script_path.parent
+db_path = project_path / 'db' / 'main.sqlite'
 
 app = FastAPI()
 
 
 # Utilize pydantic schema to perform automatic type checking of incoming data
 class Post(BaseModel):
+    id: int
     title: str
     description: str
-    rating: Optional[int] = None
+    user_id: int
 
 
-my_posts = [{
-    'title': 'Test 1',
-    'description': 'This is a post',
-    'rating': 1
-}, {
-    'title': 'Test 2',
-    'description': 'This is a post',
-    'rating': 2
-}]
+def get_connection():
+    connection = sqlite3.connect(db_path)
+    return connection
 
 
-@app.get('/')
-async def root():
+def close_connection(connection: sqlite3.Connection):
+    connection.close()
+
+
+@app.get('/', response_class=JSONResponse)
+def root():
     '''Returns Hello world for basic testing'''
     return {'message': 'Hello world!!'}
 
 
-@app.get('/items/{item_id}')
-def read_item(item_id: int, short: bool = True):
-    if not short:
-        return {'description': short}
-    return {'item_id': item_id}
+@app.get('/posts/', response_model=List[Post])
+def get_posts(connection: sqlite3.Connection = Depends(get_connection)):
+    select_posts = '''--sql
+    SELECT * from posts;
+    '''
+    cursor = connection.cursor()
+    cursor.execute(select_posts)
+    posts = cursor.fetchall()
+    cursor.close()
+
+    # Convert fetched posts to a list of Post objects
+    post_objects = []
+    for post in posts:
+        post_objects.append(
+            Post(id=post[0],
+                 title=post[1],
+                 description=post[2],
+                 user_id=post[3]))
+    return post_objects
 
 
-@app.post('/items/{item_id}')
-def create_item(item_id: int):
-    return {'message': f'Successfully created item {item_id}'}
-
-
-@app.get('/posts/')
-def get_posts():
-    return my_posts
-
-
-@app.get('/posts/{post_id}')
-def get_post(post_id: int, short: bool = True):
-    try:
-        posts = my_posts[post_id]
-
-        if not short:
-            return {'data': posts}
-        return {'title': posts['title']}
-    except IndexError:
-        print('Error: post not found')
+@app.get('/posts/{post_id}', response_model=Post)
+def get_post(post_id: int,
+             short: bool = True,
+             connection: sqlite3.Connection = Depends(get_connection)):
+    select_posts = '''--sql
+    SELECT * from posts WHERE id = ?;
+    '''
+    cursor = connection.cursor()
+    cursor.execute(select_posts, (post_id, ))
+    post = cursor.fetchone()
+    cursor.close()
+    if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail='post not found') from IndexError
+                            detail='Post not found')
+    if short:
+        return {'title': post[1]}
+    return Post(id=post[0],
+                title=post[1],
+                description=post[2],
+                user_id=post[3])
 
 
 @app.post('/posts/', status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
-    my_posts.append(post.model_dump())
+def create_post(post: Post,
+                connection: sqlite3.Connection = Depends(get_connection)):
+    create_post_query = '''--sql
+    INSERT INTO posts (title, description, user_id) VALUES (?, ?, ?);
+    '''
+    cursor = connection.cursor()
+    cursor.execute(create_post_query,
+                   (post.title, post.description, post.user_id))
+    connection.commit()
+    cursor.close()
     return {'message': f'Successfully created post {post.title}'}
 
 
 @app.put('/posts/{post_id}')
-def update_post(post_id: int, post: Post):
-    try:
-        my_posts[post_id] = post.model_dump()
-        return {'message': f'Successfully updated post {post_id}'}
-    except IndexError:
-        print('Error: post not found')
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail='post not found') from IndexError
+def update_post(post_id: int,
+                post: Post,
+                connection: sqlite3.Connection = Depends(get_connection)):
+    update_post_query = '''--sql
+    UPDATE posts SET title = ?, description = ?, user_id = ? WHERE id = ?;
+    '''
+    cursor = connection.cursor()
+    cursor.execute(update_post_query,
+                   (post.title, post.description, post.user_id, post_id))
+    connection.commit()
+    cursor.close()
+    return {'message': f'Successfully updated post {post_id}'}
 
 
 @app.delete('/posts/{post_id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(post_id: int):
-    try:
-        my_posts.pop(post_id)
-        return {'message': f'Successfully deleted the post {post_id}'}
-    except IndexError:
-        print('Error: post not found')
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail='post not found') from IndexError
-
-
-async def call_api(endpoint: str, key: str, city: str):
-    params = {'key': key, 'q': city}
-    response = requests.get(endpoint, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        temperature = data['current']['temp_c']
-        humidity = data['current']['humidity']
-        return temperature, humidity
-    else:
-        print(f'Error: {response.status_code} - {response.text}')
-        return {'error': response.text}
-
-
-@app.get('/weather/{city}')
-async def get_weather(city: str, short: bool = True):
-    '''Return current weather conditions in a given location.
-
-    Args:
-        city (str): City name
+def delete_post(post_id: int,
+                connection: sqlite3.Connection = Depends(get_connection)):
+    delete_post_query = '''--sql
+    DELETE FROM posts WHERE id = ?;
     '''
-    endpoint = 'http://api.weatherapi.com/v1/current.json'
-    key = '07ac51114175472c8ea15758230511'
-    temperature, humidity = await call_api(endpoint, key, city)
-    if not short:
-        return {'Temp': temperature, 'Humidity': humidity}
-    return {'Temp': temperature}
+    cursor = connection.cursor()
+    cursor.execute(delete_post_query, (post_id, ))
+    connection.commit()
+    cursor.close()
+    return {'message': f'Successfully deleted the post {post_id}'}
+
+
+# TODO
+# Completed get_posts however need to check other paths as they are still not passing the validation
---
+++ app/test.py
@@ -0,0 +1,108 @@
+# Import fastapi and create a default test route
+from typing import Optional
+
+import requests
+from fastapi import FastAPI, status
+from fastapi.exceptions import HTTPException
+from pydantic.main import BaseModel
+
+app = FastAPI()
+
+
+# Utilize pydantic schema to perform automatic type checking of incoming data
+class Post(BaseModel):
+    title: str
+    description: str
+    rating: Optional[int] = None
+
+
+my_posts = [{
+    'title': 'Test 1',
+    'description': 'This is a post',
+    'rating': 1
+}, {
+    'title': 'Test 2',
+    'description': 'This is a post',
+    'rating': 2
+}]
+
+
+@app.get('/')
+async def root():
+    '''Returns Hello world for basic testing'''
+    return {'message': 'Hello world!!'}
+
+
+@app.get('/posts/')
+def get_posts():
+    return my_posts
+
+
+@app.get('/posts/{post_id}')
+def get_post(post_id: int, short: bool = True):
+    try:
+        posts = my_posts[post_id]
+
+        if not short:
+            return {'data': posts}
+        return {'title': posts['title']}
+    except IndexError:
+        print('Error: post not found')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='post not found') from IndexError
+
+
+@app.post('/posts/', status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    my_posts.append(post.model_dump())
+    return {'message': f'Successfully created post {post.title}'}
+
+
+@app.put('/posts/{post_id}')
+def update_post(post_id: int, post: Post):
+    try:
+        my_posts[post_id] = post.model_dump()
+        return {'message': f'Successfully updated post {post_id}'}
+    except IndexError:
+        print('Error: post not found')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='post not found') from IndexError
+
+
+@app.delete('/posts/{post_id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(post_id: int):
+    try:
+        my_posts.pop(post_id)
+        return {'message': f'Successfully deleted the post {post_id}'}
+    except IndexError:
+        print('Error: post not found')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='post not found') from IndexError
+
+
+async def call_api(endpoint: str, key: str, city: str):
+    params = {'key': key, 'q': city}
+    response = requests.get(endpoint, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        temperature = data['current']['temp_c']
+        humidity = data['current']['humidity']
+        return temperature, humidity
+    else:
+        print(f'Error: {response.status_code} - {response.text}')
+        return {'error': response.text}
+
+
+@app.get('/weather/{city}')
+async def get_weather(city: str, short: bool = True):
+    '''Return current weather conditions in a given location.
+
+    Args:
+        city (str): City name
+    '''
+    endpoint = 'http://api.weatherapi.com/v1/current.json'
+    key = '07ac51114175472c8ea15758230511'
+    temperature, humidity = await call_api(endpoint, key, city)
+    if not short:
+        return {'Temp': temperature, 'Humidity': humidity}
+    return {'Temp': temperature}
