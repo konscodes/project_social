53829a8321067a5a0d6b8d4ef3162d66c8d71ced
Kons M <katashi.rain@gmail.com>
2024-03-04 11:10:28 +0900

    Added user table

--- app/main.py
+++ app/main.py
@@ -2,7 +2,16 @@ from fastapi import Depends, FastAPI, HTTPException, status
 from sqlmodel import Session, select
 
 from .database import create_tables, get_session
-from .models import CreatePost, PostBase, PostLimited, UpdatePost
+from .models import (
+    CreatePost,
+    CreateUser,
+    PostBase,
+    PostLimited,
+    UpdatePost,
+    UpdateUser,
+    UserBase,
+    UserLimited,
+)
 
 create_tables()
 
@@ -65,5 +74,59 @@ def delete_post(post_id: int, session: Session = Depends(get_session)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail='Post not found')
     session.delete(post)
+    session.commit()
+    return None
+
+
+# Get all users
+@app.get('/users/', response_model=list[UserLimited])
+def read_users(session: Session = Depends(get_session)):
+    statement = select(UserBase)
+    return session.exec(statement).all()
+
+
+# Get a specific user
+@app.get('/users/{user_id}', response_model=UserBase)
+def read_user(user_id: int, session: Session = Depends(get_session)):
+    statement = select(UserBase).where(UserBase.id == user_id)
+    user = session.exec(statement).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='User not found')
+    return user
+
+
+# Create a new user
+@app.post('/users/', response_model=CreateUser)
+def create_user(user: UserBase, session: Session = Depends(get_session)):
+    session.add(user)
+    session.commit()
+    return user
+
+
+# Update a user
+@app.put('/users/{user_id}', response_model=UpdateUser)
+def update_user(user_id: int,
+                data: UserBase,
+                session: Session = Depends(get_session)):
+    user = session.get(UserBase, user_id)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='User not found')
+    for key, value in data.model_dump(exclude_unset=True).items():
+        setattr(user, key, value)
+    session.add(user)
+    session.commit()
+    return user
+
+
+# Delete a user
+@app.delete('/users/{user_id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    user = session.get(UserBase, user_id)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='User not found')
+    session.delete(user)
     session.commit()
     return None
--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic.main import BaseModel
+from pydantic import BaseModel, validator
 from sqlmodel import Field, SQLModel
 
 
@@ -26,11 +26,54 @@ class PostLimited(BaseModel):
 
 # Define the schema for a response upon post creation
 class CreatePost(BaseModel):
-    title: str
     message: str = 'Successfully created post'
+    id: int
+    title: str
 
 
 # Define the schema for a response upon post creation
 class UpdatePost(BaseModel):
-    title: str
     message: str = 'Successfully updated post'
+    id: int
+    title: str
+
+
+# Define the SQLModel for the User table
+class UserBase(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(min_length=1)
+    email: str = Field(min_length=1, default=None, unique=True)
+    password: str = Field(min_length=1)
+    date_created: datetime = Field(default=datetime.now(), nullable=False)
+
+    # TODO
+    # Name field is accepted as empty which is not good, need to fix
+    # validator doesnt work as expected
+    @validator('name')
+    def name_must_not_be_empty(cls, v):
+        if v.strip() == '':
+            raise ValueError('Name cannot be an empty string')
+        return v
+
+
+# Define the schema for a general response
+class UserLimited(BaseModel):
+    id: int
+    name: str
+    email: str
+
+
+# Define the schema for a response upon user creation
+class CreateUser(BaseModel):
+    message: str = 'Successfully created user'
+    id: int
+    name: str
+    email: str
+
+
+# Define the schema for a response upon user creation
+class UpdateUser(BaseModel):
+    message: str = 'Successfully updated user'
+    id: int
+    name: str
+    email: str
